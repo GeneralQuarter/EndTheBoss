8833a6c462679ec3e63cb0173bb0a0e8c77cbb79
x1QG1x <x1q.g1x@gmail.com>
2016-02-11 15:40:51 -0500

    Cadriage fait (A tester sur différents téléphones pour le ratio...)

--- app/src/main/java/virus/endtheboss/GameViewFragment.java
+++ app/src/main/java/virus/endtheboss/GameViewFragment.java
@@ -1,7 +1,10 @@
 package virus.endtheboss;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.View;
@@ -13,25 +16,60 @@ import android.view.ViewGroup;
  */
 public class GameViewFragment extends Fragment implements SurfaceHolder.Callback{
 
+    private GameSurface gs;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.game_view, container, false);
-        //TODO
-
+        gs = new GameSurface(getActivity());
+        gs.getHolder().addCallback(this);
+        gs.setWillNotDraw(false);
+        return gs;
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        tryDrawing(holder);
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+        tryDrawing(holder);
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    private void tryDrawing(SurfaceHolder holder) {
+        Log.i("Draw", "Trying to draw...");
+
+        Canvas canvas = holder.lockCanvas();
+        if (canvas == null) {
+            Log.e("Draw", "Cannot draw onto the canvas as it's null");
+        } else {
+            drawMyStuff(canvas);
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void drawMyStuff(final Canvas canvas) {
+        int WIDTH = canvas.getWidth();
+        int HEIGHT = canvas.getHeight();
+
+        float tileHeight = HEIGHT/20f;
+        float tileWidth = WIDTH/20f;
+
+        //Background color
+        canvas.drawRGB(255, 128, 128);
+
+        //Lines
+        Paint p = new Paint();
+        p.setColor(getResources().getColor(R.color.colorPrimaryDark));
+        for(float i = 1f; i <= 19f; i++){
+            canvas.drawLine(i*tileWidth, 0, i*tileWidth, (float) HEIGHT, p);
+            canvas.drawLine(0, i*tileHeight, WIDTH, i*tileHeight, p);
+        }
 
     }
 }
